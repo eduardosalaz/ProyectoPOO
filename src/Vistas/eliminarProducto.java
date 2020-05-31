32df5971525a5b01fd7e16a83c1f8a744c07192d
MaoLugo <59459123+MaoLugo@users.noreply.github.com>
2020-05-31 02:54:01 -0500

    Update eliminarProducto.java
    Completada.

--- src/Vistas/eliminarProducto.java
+++ src/Vistas/eliminarProducto.java
@@ -20,7 +20,7 @@ public class eliminarProducto implements ActionListener{
 	private JTextField textClave;
 
 	/**
-	 * Launch the application..
+	 * Launch the application.
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
