d47a88d0fa799bcd3864118dc8e86a4d85bdd8a1
MaoLugo <59459123+MaoLugo@users.noreply.github.com>
2020-06-03 16:25:44 -0500

    Update editarFuncion.java

--- src/Interfaces/editarFuncion.java
+++ src/Interfaces/editarFuncion.java
@@ -12,11 +12,16 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
 
 public class editarFuncion implements ActionListener{
+	private int cont30 = 1,cont31 = 1;
 	private JButton btnVolver,btnActualizar;
 	public JFrame frame;
-	private JTextField textIdPelicula, textDd, textMm, textAaaa, textHh, textHoraMm;
+	private JTextField textIdPelicula;
+	private JComboBox comboBoxMm,comboBoxDd;
 
 	/**
 	 * Launch the application.
@@ -105,7 +110,7 @@ public class editarFuncion implements ActionListener{
 		JComboBox comboBoxNumero = new JComboBox();
 		comboBoxNumero.setForeground(Color.WHITE);
 		comboBoxNumero.setBackground(new Color(34, 31, 32));
-		comboBoxNumero.setBounds(182, 190, 287, 21);
+		comboBoxNumero.setBounds(196, 190, 287, 21);
 		frame.getContentPane().add(comboBoxNumero);
 		
 		JLabel lblFecha = new JLabel("Fecha:");
@@ -126,32 +131,17 @@ public class editarFuncion implements ActionListener{
 		lblEspecial.setBounds(573, 268, 170, 24);
 		frame.getContentPane().add(lblEspecial);
 		
-		JLabel lblDd = new JLabel("DD");
-		lblDd.setForeground(Color.WHITE);
-		lblDd.setFont(new Font("Arial", Font.PLAIN, 20));
-		lblDd.setBounds(111, 268, 68, 24);
-		frame.getContentPane().add(lblDd);
-		
-		textDd = new JTextField();
-		textDd.setColumns(10);
-		textDd.setBounds(145, 272, 61, 19);
-		frame.getContentPane().add(textDd);
-		
-		textMm = new JTextField();
-		textMm.setColumns(10);
-		textMm.setBounds(267, 272, 61, 19);
-		frame.getContentPane().add(textMm);
-		
-		JLabel lblMm = new JLabel("/ MM");
+		JLabel lblMm = new JLabel("MM");
 		lblMm.setForeground(Color.WHITE);
 		lblMm.setFont(new Font("Arial", Font.PLAIN, 20));
-		lblMm.setBounds(216, 268, 68, 24);
+		lblMm.setBounds(102, 268, 47, 24);
 		frame.getContentPane().add(lblMm);
 		
-		textAaaa = new JTextField();
-		textAaaa.setColumns(10);
-		textAaaa.setBounds(408, 272, 61, 19);
-		frame.getContentPane().add(textAaaa);
+		JLabel lblDd = new JLabel("/ DD");
+		lblDd.setForeground(Color.WHITE);
+		lblDd.setFont(new Font("Arial", Font.PLAIN, 20));
+		lblDd.setBounds(216, 268, 68, 24);
+		frame.getContentPane().add(lblDd);
 		
 		JLabel lblAaaa = new JLabel("/ AAAA");
 		lblAaaa.setForeground(Color.WHITE);
@@ -171,48 +161,110 @@ public class editarFuncion implements ActionListener{
 		lblHh.setBounds(89, 350, 68, 24);
 		frame.getContentPane().add(lblHh);
 		
-		textHh = new JTextField();
-		textHh.setColumns(10);
-		textHh.setBounds(118, 353, 61, 19);
-		frame.getContentPane().add(textHh);
-		
 		JLabel lblHoraMm = new JLabel(": MM");
 		lblHoraMm.setForeground(Color.WHITE);
 		lblHoraMm.setFont(new Font("Arial", Font.PLAIN, 20));
-		lblHoraMm.setBounds(182, 350, 68, 24);
+		lblHoraMm.setBounds(185, 350, 68, 24);
 		frame.getContentPane().add(lblHoraMm);
 		
-		textHoraMm = new JTextField();
-		textHoraMm.setColumns(10);
-		textHoraMm.setBounds(233, 353, 61, 19);
-		frame.getContentPane().add(textHoraMm);
+		comboBoxMm = new JComboBox();
+		comboBoxMm.setModel(new DefaultComboBoxModel(new String[] {"01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"}));
+		comboBoxMm.setForeground(Color.WHITE);
+		comboBoxMm.setBackground(new Color(34, 31, 32));
+		comboBoxMm.setBounds(143, 270, 63, 21);
+		frame.getContentPane().add(comboBoxMm);
+		comboBoxMm.addActionListener(this);
+		
+		comboBoxDd = new JComboBox();
+		comboBoxDd.setModel(new DefaultComboBoxModel(new String[] {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"}));
+		comboBoxDd.setForeground(Color.WHITE);
+		comboBoxDd.setBackground(new Color(34, 31, 32));
+		comboBoxDd.setBounds(265, 268, 63, 21);
+		frame.getContentPane().add(comboBoxDd);
+		
+		JComboBox comboBoxAaaa = new JComboBox();
+		comboBoxAaaa.setModel(new DefaultComboBoxModel(new String[] {"2020"}));
+		comboBoxAaaa.setForeground(Color.WHITE);
+		comboBoxAaaa.setBackground(new Color(34, 31, 32));
+		comboBoxAaaa.setBounds(406, 268, 63, 21);
+		frame.getContentPane().add(comboBoxAaaa);
+		
+		JSpinner spinner = new JSpinner();
+		spinner.setModel(new SpinnerNumberModel(1, 1, 24, 1));
+		spinner.setBounds(124, 354, 51, 20);
+		frame.getContentPane().add(spinner);
+		
+		JSpinner spinner_1 = new JSpinner();
+		spinner_1.setModel(new SpinnerNumberModel(0, 0, 59, 1));
+		spinner_1.setBounds(238, 354, 51, 20);
+		frame.getContentPane().add(spinner_1);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		menuFunciones menuFunciones = new menuFunciones();
+		funciones funcion = new funciones();
 		
-		if(e.getSource()==btnActualizar) {
-			int opcion = JOptionPane.showConfirmDialog(null, "Se ha actualizado exitosamente!. \n\n¿Quiere actualizar otra funcion?\n", "Actualizado",JOptionPane.YES_NO_OPTION);
+		
+		if(e.getSource()==comboBoxMm) {
+			
+			String deMm = (String) comboBoxMm.getSelectedItem();
+			if(deMm == "02") {
+				if(cont31==1 && cont30==1) {
+					comboBoxDd.removeItem("30");
+					comboBoxDd.removeItem("31");
+					cont30=0;
+					cont31=0;
+				}
+				else if(cont31==0 && cont30==1) {
+					comboBoxDd.removeItem("30");
+					cont30=0;
+				}	
+			}	
+			else if(deMm=="01" || deMm=="03" || deMm=="05" || deMm=="07" || deMm=="08" || deMm=="10" || deMm=="12") {
+				
+				if(cont30==0 && cont31==0) {
+					comboBoxDd.addItem("30");
+					comboBoxDd.addItem("31");
+					cont30=1;
+					cont31=1;
+				}
+				else if(cont30==1 && cont31==0) {
+					comboBoxDd.addItem("31");
+					cont31=1;
+				}
+			}
+			else if(deMm=="04" || deMm=="06" || deMm=="09" || deMm=="11") {
+				
+				if(cont30==0 && cont31==0) {
+					comboBoxDd.addItem("30");
+					cont30=1;
+				}
+				else if(cont30==1 && cont31==1) {
+					comboBoxDd.removeItem("31");
+					cont31=0;
+				}
+			}
+		}
+		else if(e.getSource()==btnVolver) {
+			funcion.frame.setVisible(true);
+			frame.dispose();
+		}
+		else if(e.getSource()==btnActualizar) {
+			int opcion = JOptionPane.showConfirmDialog(null, "Se ha actualizado exitosamente!. \n\nÂ¿Quiere actualizar otra funcion?\n", "Actualizado",JOptionPane.YES_NO_OPTION);
 			
 			if(opcion==JOptionPane.YES_OPTION) {
 				textIdPelicula.setText("");
-				textDd.setText("");
-				textMm.setText("");
-				textAaaa.setText("");
-				textHh.setText("");
-				textHoraMm.setText("");
+		
 			}
 			else {
-				menuFunciones.frame.setVisible(true);
+				funcion.frame.setVisible(true);
 				frame.dispose();
 			}
 		}
 		else {
-			menuFunciones.frame.setVisible(true);
+			funcion.frame.setVisible(true);
 			frame.dispose();
 		}
 		
 	}
-
 }
