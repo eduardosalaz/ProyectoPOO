01c2fbea7e16bb0de2cd195b1d113ec84e426c30
MaoLugo <59459123+MaoLugo@users.noreply.github.com>
2020-06-04 02:34:03 -0500

    Update Inventario.java

--- src/Interfaces/Inventario.java
+++ src/Interfaces/Inventario.java
@@ -1,25 +1,40 @@
-package Interfaces;
+	package Interfaces;
 
 import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JTable;
+import javax.swing.JTextField;
 
 public class Inventario extends JFrame implements ActionListener {
 
     private JPanel contentPane;
-    private JTable table;
-    private JButton btn_volver;
+    JList list = new JList();
+    private JButton btn_volver, btnBuscar;
     public JFrame frame;
-
+    private JTextField textIdProducto;
+    DefaultListModel lista;
+    private Connection con = ConexionBD.conectar();
+    public PreparedStatement pstm = null;
+    ResultSet rs = null;
+    String query="";
+    JScrollPane jp = new JScrollPane(list);
+    
     /**
      * Launch the application.
      */
@@ -51,8 +66,29 @@ public class Inventario extends JFrame implements ActionListener {
         JLabel lbl_inventario = new JLabel("Inventario");
         lbl_inventario.setForeground(Color.WHITE);
         lbl_inventario.setFont(new Font("Arial", Font.BOLD, 40));
-        lbl_inventario.setBounds(376, 11, 191, 88);
+        lbl_inventario.setBounds(400, 11, 191, 88);
         frame.getContentPane().add(lbl_inventario);
+        
+        JLabel lblIdProducto = new JLabel("ID Producto:");
+        lblIdProducto.setForeground(Color.WHITE);
+        lblIdProducto.setFont(new Font("Arial", Font.PLAIN, 20));
+        lblIdProducto.setBounds(280, 120, 149, 16);
+        frame.getContentPane().add(lblIdProducto);
+        
+        textIdProducto = new JTextField();
+        textIdProducto.setColumns(10);
+        textIdProducto.setBounds(400, 120, 208, 19);
+        frame.getContentPane().add(textIdProducto);
+        
+        btnBuscar = new JButton("Buscar");
+        btnBuscar.setForeground(Color.WHITE);
+        btnBuscar.setFont(new Font("Arial", Font.PLAIN, 17));
+        btnBuscar.setFocusPainted(false);
+        btnBuscar.setBorderPainted(false);
+        btnBuscar.setBackground(new Color(171, 0, 51));
+        btnBuscar.setBounds(413, 170, 149, 39);
+        frame.getContentPane().add(btnBuscar);
+        btnBuscar.addActionListener(this);
 
         btn_volver = new JButton("Volver");
         btn_volver.setForeground(Color.WHITE);
@@ -63,10 +99,17 @@ public class Inventario extends JFrame implements ActionListener {
         btn_volver.setBounds(10, 562, 139, 31);
 		frame.getContentPane().add(btn_volver);
 		btn_volver.addActionListener(this);
-
-        table = new JTable();
-        table.setBounds(10, 110, 924, 425);
-        frame.getContentPane().add(table);
+		
+		frame.getContentPane().add(jp);
+        
+		lista = new DefaultListModel();
+		list.setModel(lista);
+        list.setBounds(10, 250, 924, 290);
+        frame.getContentPane().add(list);
+        list.add(jp);
+     
+        list.setVisibleRowCount(4);
+        lista.addElement("Nombre                                Tamaño                                Precio                                Sabor                                Existencias");
     }
 
     @Override
@@ -75,6 +118,36 @@ public class Inventario extends JFrame implements ActionListener {
             Informe informe = new Informe();
             informe.frame.setVisible(true);
             frame.dispose();
+        }
+        else if(e.getSource()==btnBuscar){
+        	try {
+        		query = "SELECT * FROM `Producto Dulceria` WHERE `ID_Producto` = ?";
+            	pstm = con.prepareStatement(query);
+            	pstm.setInt(1, Integer.parseInt(textIdProducto.getText()));
+            	
+            	rs = pstm.executeQuery(); 
+            	
+            	if(rs.next()) {
+            		String nom = rs.getString("Nombre_Producto");
+            		String tam = rs.getString("Tamaño");
+            		String prec = rs.getString("Precio");
+            		String sabor = rs.getString("Sabor");
+            		String exis = rs.getString("Existencias");
+            		
+            		lista.addElement(" ");
+                	lista.addElement(nom+"                                "+tam+"                                "+prec+"                                "+sabor+"                                "+exis);
+                	lista.addElement("_______________________________________________________________________________________________________________________");
+            	}
+            	
+            	
+            	
+            	
+            } catch (SQLException e1) {
+            	
+            
+            	
+            		
+            }
         }
     }
 }
